158dd07f3eb43784d4a23c04a292ee5ebab87678
IKFTC <155031885+IKFTC@users.noreply.github.com>
2025-04-10 16:09:49 +0800

    distance calculation  updated

--- TeamCode/src/main/java/subsystems/SleepyStuffff/Math/VisionUtil.java
+++ TeamCode/src/main/java/subsystems/SleepyStuffff/Math/VisionUtil.java
@@ -2,10 +2,10 @@ package subsystems.SleepyStuffff.Math;
 
 public class VisionUtil {
     public static double yDistance(double ty) {
-        return 0.016 * ty * ty * ty - 0.017 * ty * ty + 11.716 * ty + 176.876;
+        return 0.0186 * ty * ty * ty - 0.0599 * ty * ty + 12.334 * ty + 256.52;
     }
     public static double xDistance(double tx, double ty) {
-        return (0.2742 * ty + 7.3) * tx;
+        return (0.0006 * ty * ty * ty - 0.0058 * ty * ty + 0.2147 * ty + 8.3889) * tx;
     }
     public static double getIntakeDegree(double xDis) {
         return Math.toDegrees(Math.asin(xDis / 180.0));
