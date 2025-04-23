d58b33fe6b9b3239dc80a317d71ed1ca4ebc0a2f
IKFTC <155031885+IKFTC@users.noreply.github.com>
2025-04-23 14:57:09 +0800

    mapping method updated

--- TeamCode/src/main/java/subsystems/SleepyStuffff/Math/VisionUtil.java
+++ TeamCode/src/main/java/subsystems/SleepyStuffff/Math/VisionUtil.java
@@ -6,15 +6,16 @@ public class VisionUtil {
 
     public static double yDistance(double ty) {
         double result = 0.369 * Math.pow(ty, 2) + 12.49 * ty + 243.5; //originally 253.5
-        if (ty >= 10 && ty < 11) {
-            result += 11 * (ty - 10);
-        }
+//        if (ty >= 10 && ty < 11) {
+//            result += 11 * (ty - 10);
+//        }
         return result;
     }
     public static double xDistance(double tx, double ty) {
         double result = (0.0008 * Math.pow(ty, 3) - 0.0005 * Math.pow(ty, 2) + 0.1778 * ty + 8.6207) * tx;
         if (result > 0) {
-            result -= 20;
+            result = result * 0.9 - 20;
+//             result -= 20;
         }
         //this is basically correct, let the robot take its time.
         return result;
@@ -24,6 +25,6 @@ public class VisionUtil {
     }
     public static double getExtendPercent(double xDis, double yDis) {
         extendLength = yDis - Math.cos(Math.asin(xDis / 180.0)) * 180;
-        return 0.000002 * Math.pow(extendLength,3) - 0.0012 * Math.pow(extendLength, 2) + 0.4 * extendLength - 2.866;
+        return (0.000002 * Math.pow(extendLength,3) - 0.0012 * Math.pow(extendLength, 2) + 0.4 * extendLength - 2.866) * 1.01;
     }
 }
