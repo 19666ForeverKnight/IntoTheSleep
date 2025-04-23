9369069c181a6176fab6ab53abe18b6223b751af
IKFTC <155031885+IKFTC@users.noreply.github.com>
2025-04-23 14:52:31 +0800

    add vertical filter

--- TeamCode/src/main/java/subsystems/IntakeSubsystem/Vision.java
+++ TeamCode/src/main/java/subsystems/IntakeSubsystem/Vision.java
@@ -1,7 +1,5 @@
 package subsystems.IntakeSubsystem;
 
-import static constants.RobotConstants.INTAKE_CLAW_ARM_INTAKE_UP;
-import static subsystems.SleepyStuffff.Math.helperAndConverter.getDetectorClassIDHelper;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
@@ -12,25 +10,31 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import constants.RobotConstants;
 import constants.RobotConstants.AllianceColour;
 import subsystems.SleepyStuffff.Math.VisionUtil;
 import subsystems.SleepyStuffff.Util.DetectorLLResultPair;
 
-public class Vision extends SubsystemBase {
+public class Vision {
     private static Vision instance;
     public Limelight3A limelight;
     private DetectorLLResultPair AppleTypePair;
     double turretDegree = 90, xDis = 0, intakePercentage = 0, rotateDegree = 0;
     double yDis = 0;
+    public double withoutX = 100, withoutY = 100;
 
-    public void Init(HardwareMap hardwareMap, Telemetry telemetry){
+    public void init(HardwareMap hardwareMap, Telemetry telemetry){
+        limelight = hardwareMap.get(Limelight3A.class, "Limelight");
+        limelight.pipelineSwitch(0);
+        this.goWork();
+    }
+
+    public void initTest(HardwareMap hardwareMap, Telemetry telemetry){
         limelight = hardwareMap.get(Limelight3A.class, "Limelight");
         limelight.pipelineSwitch(5);
         this.goWork();
     }
+
     public List<Double> getLatestResult(AllianceColour a, boolean includeYellow) {
         LLResult hxg = limelight.getLatestResult();
         for (int b = 0; b < 34; b++) {
@@ -42,7 +46,30 @@ public class Vision extends SubsystemBase {
             List<DetectorResult> unfilteredResults = hxg.getDetectorResults();
             String detectorID = a.name().toLowerCase();
             for (DetectorResult result : unfilteredResults) {
-                if (result.getClassName().equals(detectorID) || (includeYellow && result.getClassName().equals("yellow")) && !result.getTargetCorners().isEmpty()) {
+                if (result.getTargetYDegrees() > -1.16 && result.getTargetYDegrees() < 15 && Math.abs(result.getTargetXDegrees() - withoutX) > 5 && Math.abs(result.getTargetYDegrees() - withoutY) > 5 && (result.getClassName().equals(detectorID) || (includeYellow && result.getClassName().equals("yellow"))) && !result.getTargetCorners().isEmpty()) {
+                    xDis = VisionUtil.xDistance(result.getTargetXDegrees(), result.getTargetYDegrees());
+                    yDis = VisionUtil.yDistance(result.getTargetYDegrees());
+                    turretDegree = 90 + VisionUtil.getIntakeDegree(xDis);
+                    intakePercentage = VisionUtil.getExtendPercent(xDis, yDis);
+
+                    List<List<Double>> corners = result.getTargetCorners();
+                    if ((corners.get(1).get(0) - corners.get(0).get(0)) / (corners.get(3).get(1) - corners.get(0).get(1)) <= 1.1) {
+                        rotateDegree = 180 - turretDegree;
+                        withoutX = result.getTargetXDegrees();
+                        withoutY = result.getTargetYDegrees();
+                        return new ArrayList<Double>() {{
+                            add(turretDegree);
+                            add(intakePercentage);
+                            add(rotateDegree);
+                        }};
+                    }
+                }
+            }
+
+            for (DetectorResult result : unfilteredResults) {
+                if (result.getTargetYDegrees() > -1.16 && result.getTargetYDegrees() < 15 && Math.abs(result.getTargetXDegrees() - withoutX) > 5 && Math.abs(result.getTargetYDegrees() - withoutY) > 5 && (result.getClassName().equals(detectorID) || (includeYellow && result.getClassName().equals("yellow"))) && !result.getTargetCorners().isEmpty()) {
+                    withoutX = result.getTargetXDegrees();
+                    withoutY = result.getTargetYDegrees();
                     xDis = VisionUtil.xDistance(result.getTargetXDegrees(), result.getTargetYDegrees());
                     yDis = VisionUtil.yDistance(result.getTargetYDegrees());
                     turretDegree = 90 + VisionUtil.getIntakeDegree(xDis);
@@ -50,7 +77,7 @@ public class Vision extends SubsystemBase {
 
                     List<List<Double>> corners = result.getTargetCorners();
                     if (((corners.get(1).get(0) - corners.get(0).get(0)) / (corners.get(3).get(1) - corners.get(0).get(1))) > (Math.abs(result.getTargetXDegrees()) > 6
-                            ? 1.4 : (Math.abs(result.getTargetXDegrees()) > 2 ? 1.2 : 1))) {
+                            ? 1.17 : (Math.abs(result.getTargetXDegrees()) > 2 ? 1.1 : 1))) {
                         rotateDegree = turretDegree > 90 ? 270 - turretDegree : 90 - turretDegree;
                     } else {
                         rotateDegree = 180 - turretDegree;
@@ -69,6 +96,7 @@ public class Vision extends SubsystemBase {
             add(0.0);
         }};
     }
+
     public void RIP(){
         limelight.stop();
     }
