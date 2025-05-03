242a3299853d1e8cedad583cf8cc3dd0f6c926e7
IKFTC <155031885+IKFTC@users.noreply.github.com>
2025-05-03 22:14:11 +0800

    Update ledTest.java

--- TeamCode/src/main/java/teleop/ledTest.java
+++ TeamCode/src/main/java/teleop/ledTest.java
@@ -1,27 +1,31 @@
 package teleop;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Config
 @TeleOp
 public class ledTest extends LinearOpMode {
     Servo led = null;
+    public static double light = 0.0;
+    public static int waitTime = 150;
     @Override
     public void runOpMode() throws InterruptedException {
         led = hardwareMap.get(Servo.class, "led");
 
         waitForStart();
         while (opModeIsActive()) {
-            led.setPosition(1);
-//            sleep(500);
-//            led.setPosition(0.5);
-//            sleep(500);
-//            led.setPosition(0);
-//            sleep(500);
-//            led.setPosition(0.5);
-//            sleep(500);
+            if (waitTime == 0) {
+                led.setPosition(light);
+            } else {
+                light += 0.05;
+                if (light > 1) light -= 1;
+                led.setPosition(light);
+                sleep(waitTime);
+            }
         }
     }
 }
